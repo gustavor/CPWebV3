9dfe32b6039783fe760d77f8229c21ed8a219cd7
Adriano C. de Moura <adrianodemoura@gmail.com>
2010-11-13 03:12:25 -0200

    início da implementação do cpwebCrud

--- app/controllers/cidades_controller.php
+++ app/controllers/cidades_controller.php
@@ -22,6 +22,123 @@
 
 class CidadesController extends AppController {
 
+	/**
+	 * nome
+	 * @var string
+	 * @access public
+	 */
 	public $name = 'Cidades';
-	public $scaffold;
+	
+	/**
+	 * Model
+	 * @var string
+	 * @access public
+	 */
+	public $uses = 'Cidade';
+	
+	/**
+	 * componentes
+	 * @var array Componentes
+	 * @access public
+	 */
+	public $components	= array('CpwebCrud','Session');
+	
+	/**
+	 * Método a ser executado antes de todos
+	 *
+	 * @access public
+	 * @return void
+	 */
+	 public function beforeFilter()
+	 {
+		 $this->viewVars['campos']['Cidade.nome']['label']['text'] 			= 'Cidade';
+		 $this->viewVars['campos']['Estado.uf']['label']['text'] 			= 'Uf';
+		 $this->viewVars['campos']['Cidade.modified']['label']['text'] 		= 'Data da Última Atualiazação';
+		 $this->viewVars['campos']['Cidade.created']['label']['text'] 		= 'Data de Criação';
+	 }
+	
+	/**
+	 * método start
+	 */
+	public function index()
+	{
+		$this->redirect('listar');
+	}
+	
+	public function beforeRender()
+	{
+		//echo '<pre>'.print_r($this->data,true).'</pre>';
+		if (isset($this->data[1]['Estado']['nome']))
+		{
+			if ($this->data[1]['Estado']['nome']=='Rio de Janeiro')
+			{
+				/* $this->viewVars['campos']['Estado.uf']['estilo_td'] 			= 'style="background-color: #ddd;"';
+				$this->viewVars['campos']['Estado.uf']['estilo_td_3_uf'] 		= 'style="background-color: green;"';
+				$this->viewVars['lista']['estilo_tr_3'] 			= 'style="background-color: #ddd;"';
+				*/
+			}
+		}
+	}
+	
+	/**
+	 * Lista os dados em paginação
+	 * 
+	 * @parameter integer 	$pag 		Número da página
+	 * @parameter string 	$ordem 		Campo usado no order by da sql
+	 * @parameter string 	$direcao 	Direção ASC ou DESC
+	 * @return void
+	 */
+	public function listar($pag=1,$ordem=null,$direcao='DESC')
+	{
+		$this->viewVars['listaCampos'] 									= array('Cidade.nome','Estado.uf','Cidade.modified','Cidade.created');
+		$this->viewVars['campos']['Cidade.modified']['estilo_th'] 		= 'width="220px"';
+		$this->viewVars['tamLista'] 									= '80%';
+		$this->CpwebCrud->listar($pag,$ordem,$direcao);
+		//$this->viewVars['campos']['Estado.uf']['estilo_td'] 			= 'style="background-color: #ddd;"';
+	}
+	
+	/**
+	 * método para edição
+	 */
+	public function editar($id=null)
+	{
+		$this->CpwebCrud->editar($id);
+	}
+	
+	/**
+	 * método para novo
+	 */
+	public function novo()
+	{
+		$this->CpwebCrud->novo();
+	}
+	
+	/**
+	 * método para exclusão
+	 */
+	public function excluir($id = null)
+	{
+		$this->CpwebCrud->excluir($id);
+	}
+	
+	/**
+	 * método para impressão
+	 **/
+	public function imprimir($id=null)
+	{
+		$this->CpwebCrud->imprimir($id);
+	}
+	
+	/**
+	 * Realiza uma pesquisa no banco de dados
+	 * 
+	 * @parameter string $texto Texto de pesquisa
+	 * @parameter string $campo Campo de pesquisa
+	 * @return void
+	 */
+	public function pesquisar($texto='',$campo=null)
+	{
+		$this->CpwebCrud->pesquisar($texto,$campo);
+	}
+
 }
